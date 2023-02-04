2e705ad0667dd6562790eb0b0a590b9919261387
mxtlrr <ruson0914@gmail.com>
2023-02-04 16:57:34 -0600

    IDT works but inf. loop breaks it

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,8 +1,9 @@
 #include "multiboot.h"
 #include "libc/stdio.h"
 
-/* arch */
+/* low level stuff */
 #include "arch/gdt.h"
+#include "arch/idt.h"
 
 
 void kernel_main(multiboot_info_t* mbd, uint32_t magic){
@@ -10,8 +11,11 @@ void kernel_main(multiboot_info_t* mbd, uint32_t magic){
 	set_fb((uint32_t*)mbd->framebuffer_addr);
 
 	printf("Hello World\n");
-	uint32_t gdtr = init_gdt();
+	uint32_t gdtr = init_gdt()+0x20;
+	uint32_t idtr = idt_init()+0x10;
 	printf("GDT was loaded at 0x%x\n", gdtr);
+	printf("IDT was loaded at 0x%x\n", idtr);
+
 
 	for(;;) asm("hlt");
 }
