3f2e7fb34a4a8548cc29846dd90c7f83eddc62b3
mxtlrr <ruson0914@gmail.com>
2023-02-04 16:25:31 -0600

    setup headers for the GDT

---
+++ include/arch/asm/gdt_.s
@@ -0,0 +1,15 @@
+[global flush_gdt]
+flush_gdt:
+  mov eax, [esp+4]
+  lgdt [eax]
+
+  jmp 0x08:.flush
+.flush:
+  ;; update descriptors
+  mov ax, 0x10
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+  ret
---
+++ include/arch/gdt.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#pragma once
+
+#include <stdint.h>
+
+#define GRANULARITY 0xcf
+
+struct gdt_entry_struct {
+	uint16_t limit_low;
+	uint16_t base_low;
+	uint8_t  base_middle;
+	uint8_t  access;
+	uint8_t  granularity;
+	uint8_t  base_high;
+}__attribute__((packed));
+typedef struct gdt_entry_struct gdt_entry_t;
+
+struct gdt_ptr_struct {
+	uint16_t limit;
+	uint32_t base;
+}__attribute__((packed));
+typedef struct gdt_ptr_struct gdt_ptr_t;
+
+
+void gdt_set_gate(int, uint32_t, uint32_t, uint8_t, uint8_t);
+void init_gdt();
+
+extern void flush_gdt(uint32_t);
