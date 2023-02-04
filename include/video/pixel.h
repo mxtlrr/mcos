7040e4f7925c4b98eda2d870f3c0a0c2c07fcec4
mxtlrr <ruson0914@gmail.com>
2023-02-04 15:40:16 -0600

    putpixel

---
+++ include/video/pixel.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define RED   0xff0000
+#define GREEN 0x00ff00
+#define BLUE  0x0000ff
+
+
+#define PITCH 4096
+
+void putpixel(uint32_t* addr, int x, int y, uint32_t color);
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,21 +3,15 @@
 #include <stdint.h>
 
 #include "multiboot.h"
-
-#define WIDTH 1024
-
-void kernel_main(multiboot_info_t* mbd, uint32_t f){
-	if(f != MULTIBOOT_BOOTLOADER_MAGIC) for(;;);
-	int x = 20;
-	int y = 40;
-	int color = 0x06;
+#include "video/pixel.h"
 
 
-	unsigned char* screen = (unsigned char*)mbd->framebuffer_addr;
-	unsigned where = x*4 + y*mbd->framebuffer_pitch;
-	screen[where] = color & 255;              // BLUE
-	screen[where + 1] = (color >> 8) & 255;   // GREEN
-	screen[where + 2] = (color >> 16) & 255;  // RED
+void kernel_main(multiboot_info_t* mbd, uint32_t magic){
+	if(magic != MULTIBOOT_BOOTLOADER_MAGIC) return;
+	uint32_t* addr = (uint32_t*)mbd->framebuffer_addr;
+
+	for(int i = 15; i != 31; i++)
+		putpixel(addr, i, 27, GREEN);
   
 	for(;;) asm("hlt");
 }
