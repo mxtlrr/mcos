785f6db81a43cab74c3f4e7cd433bf9d30ac820a
mxtlrr <ruson0914@gmail.com>
2023-02-09 14:00:32 -0600

    try getting mmap

--- qemu.sh
+++ qemu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 TARGET=i386
-qemu-system-$TARGET -cdrom myos.iso -m 256 -no-shutdown -no-reboot -serial stdio
+qemu-system-$TARGET -cdrom myos.iso -no-shutdown -no-reboot -serial stdio
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -12,11 +12,33 @@
 #include "arch/i386/irq/irq1.h"
 
 void kernel_main(multiboot_info_t* mbd, uint32_t magic){
-	if(magic != MULTIBOOT_BOOTLOADER_MAGIC) return;
+	if(magic != MULTIBOOT_BOOTLOADER_MAGIC
+		&& !(mbd->flags >> 6 & 0x1)) return;
 	set_fb((uint32_t*)mbd->framebuffer_addr);
 	set_pitch(mbd->framebuffer_pitch);
 
-	printf("Hello World\n");
+	uint16_t total = 0;
+	uint8_t lowmem, highmem = 0;
+	outb(0x70, 0x30);
+	lowmem = inb(0x71);
+	outb(0x70, 0x31);
+	highmem = inb(0x71);
+
+	total = lowmem|highmem << 8;
+	printf("From CMOS:\nLow Memory: %d\nHigh Memory: %d\nTotal: %d\n",
+		lowmem, highmem, total);
+
+
+	printf("\nFrom GRUB:\n");
+	for(int i = 0; i < mbd->mmap_length; i+= sizeof(multiboot_memory_map_t)){
+		multiboot_memory_map_t* mmmt = (multiboot_memory_map_t*)mbd->mmap_addr+i;
+
+		printf("Starts at: 0x%x (high %x) | 0x%x bytes long | Size: 0x%x | Type: %d\n",
+					mmmt->addr_low, mmmt->addr_high, mmmt->len_low, mmmt->size, mmmt->type);
+	}
+
+
+	printf("\nHello World\n");
 	uint32_t gdtr = init_gdt()+0x20;
 	uint32_t idtr = init_idt()+0x20;
 	printf("GDT was loaded at 0x%x\n", gdtr);
